d2c27d04440b4e9aa4eaa7a77fc3d072f857dc75
brenoricco <brenoriccodev@gmail.com>
2023-01-01 20:33:30 -0300

    Removendo cabeçalho padrao da rota

--- App.tsx
+++ App.tsx
@@ -3,8 +3,8 @@ import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 import { THEME } from './src/theme';
-import { SignUp } from '@screens/SignUp';
 import { Loading } from '@components/Loading';
+import { Routes } from '@routes/index';
 
 export default function App() {
   const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold});
@@ -17,7 +17,7 @@ export default function App() {
         translucent
       />
       
-      { fontsLoaded ? <SignUp /> : <Loading /> }
+      { fontsLoaded ? <Routes /> : <Loading /> }
     </NativeBaseProvider>
   );
 }
