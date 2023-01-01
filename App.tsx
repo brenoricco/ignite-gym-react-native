621a3db67f4374fa295e15655651a86bd68349ad
brenoricco <brenoriccodev@gmail.com>
2023-01-01 19:29:51 -0300

    Imagem de Background da tela de SignIn

--- App.tsx
+++ App.tsx
@@ -1,8 +1,10 @@
 import { StatusBar, Text, View } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
-import { Loading } from '@assets/components/Loading';
+
 import { THEME } from './src/theme';
+import { SignIn } from '@screens/SignIn';
+import { Loading } from '@components/Loading';
 
 export default function App() {
   const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold});
@@ -15,7 +17,7 @@ export default function App() {
         translucent
       />
       
-      <Loading />
+      { fontsLoaded ? <SignIn /> : <Loading /> }
     </NativeBaseProvider>
   );
 }
