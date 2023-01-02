824da461ef5cefb47304d2904cb235ef2909b863
brenoricco <brenoriccodev@gmail.com>
2023-01-02 20:32:54 -0300

    Componente UserPhoto utilizando uma imagem externa da web

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,8 +1,16 @@
 import { Heading, HStack, Text, VStack } from "native-base";
+import { UserPhoto } from "./UserPhoto";
 
 export function HomeHeader() {
     return (
         <HStack bgColor="gray.600" pt={16} pb={5} px={8} alignItems="center">
+            <UserPhoto 
+                source={{ uri: 'https://github.com/brenoricco.png' }}
+                size={16}
+                alt="Imagem do perfil do usuário"
+                mr={4}
+            />
+
             <VStack>
                 <Text color="gray.100" fontSize="md">
                     Olá,
