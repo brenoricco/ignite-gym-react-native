e0e8eeb9a016b27907c9d3a081a623d24f6da0ba
brenoricco <brenoriccodev@gmail.com>
2023-01-03 20:03:30 -0300

    Recuperando e exibindo a foto selecionada

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -12,14 +12,21 @@ const PHOTO_SIZE = 33;
 
 export function Profile() {
     const [photoIsLoading, setPhotoIsLoading] = useState(false);
+    const [userPhoto, setUserPhoto] = useState<string | undefined>('https://github.com/brenoricco.png');
 
     async function handleUserPhotoSelect() {
-        await ImagePicker.launchImageLibraryAsync({
+        const photoSelected = await ImagePicker.launchImageLibraryAsync({
             mediaTypes: ImagePicker.MediaTypeOptions.Images,
             quality: 1,
             aspect: [4, 4],
             allowsEditing: true
         });
+
+        if(photoSelected.canceled) {
+            return
+        }
+
+        setUserPhoto(photoSelected.assets[0].uri);
     }
 
     return (
@@ -38,7 +45,7 @@ export function Profile() {
                         />
                         :
                         <UserPhoto
-                            source={{ uri: 'https://github.com/brenoricco.png' }}
+                            source={{ uri: userPhoto }}
                             alt="Foto do usuário"
                             size={PHOTO_SIZE}
                         />
