1e0cabfc164dc2431a7bb50295d676e706c68f3d
brenoricco <brenoriccodev@gmail.com>
2023-01-03 20:17:02 -0300

    Utilizando FileSystem, buncando informações do arquivo e restringindo tamanho da foto selecionada.

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,10 +1,11 @@
 import { Center, ScrollView, VStack, Skeleton, Text, Heading } from 'native-base';
 import * as ImagePicker from 'expo-image-picker';
+import * as FileSystem from 'expo-file-system';
 
 import { ScreenHeader } from '@components/ScreenHeader';
 import { UserPhoto } from '@components/UserPhoto';
 import { useState } from 'react';
-import { TouchableOpacity } from 'react-native';
+import { Alert, TouchableOpacity } from 'react-native';
 import { Input } from '@components/Input';
 import { Button } from '@components/Button';
 
@@ -30,6 +31,12 @@ export function Profile() {
             }
 
             if(photoSelected.assets.length && photoSelected.assets[0].uri) {
+                const photoInfo = await FileSystem.getInfoAsync(photoSelected.assets[0].uri);
+
+                if(photoInfo.size && (photoInfo.size / 1024 / 1024) > 5) {
+                    return Alert.alert("Essa imagem é muito grande. Escolha uma de até 5MB.");
+                }
+
                 setUserPhoto(photoSelected.assets[0].uri);
             }    
             
